0f69cd4b602a5e043de0cd5f26e0622c84ec4d20
KarinaNiebles <fkariniebles@gmail.com>
2018-06-19 06:59:30 -0500

    cambiando el data.js y main.js

---
+++ src/data.js
@@ -0,0 +1,4 @@
+window.computeUserStats = (users,progress,courses) =>{};
+window.sortUsers = (users, orderBy, orderDirection) =>{};
+window.filterUsers = (users,filterBy) =>{};
+window.processCohortData = ({ cohortsData, orderBy,orderdirection,filterBy}) =>{};
--- src/main.js
+++ src/main.js
@@ -1,67 +1,55 @@
+const userUrl = "../data/cohorts/lim-2018-03-pre-core-pw/users.json";
+const progress = "../data/cohorts/lim-2018-03-pre-core-pw/progress.json";
+const cohortUrl = "../data/cohorts.json";
+
+const dataRequest = (url, callback) => {
+	const xhr = new XMLHttpRequest();
+	xhr.open("GET", url);
+	xhr.onload = callback;
+	xhr.send();
+}
+
+const getCohort = () => {
+	let arrayCohorts = JSON.parse(event.target.responseText);
+	let select = document.getElementById("cboCohorts");
+	for (let i in arrayCohorts) {
+		let option = document.createElement("option");
+		select.options.add(option, 0);
+		select.options[0].value = arrayCohorts[i].id;
+		select.options[0].innerText = arrayCohorts[i].id;
+	}
+};
+const getUsers = () => {
+
+	let arrayUsers = JSON.parse(event.target.responseText);
+	let select = document.getElementById("cboUsers");
+	for (let i in arrayUsers) {
+		let option = document.createElement("option");
+		select.options.add(option, 0);
+		select.options[0].value = arrayUsers[i].id;
+		select.options[0].innerText = arrayUsers[i].name;
+	}
+};
 let cboUsers = document.getElementById('cboUsers');
 cboUsers.addEventListener("change", function () {
-    getProgress();
+	getProgress();
 });
 
-getCohort();
-computeUsersStats(); function getCohort() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState === 4 && xhttp.status === 200) {
-            let arrayCohorts = JSON.parse(xhttp.responseText);
-            // console.log(arrayCohorts);
-            let select = document.getElementById("cboCohorts");
-            for (let i in arrayCohorts) {
-                let option = document.createElement("option");
-                select.options.add(option, 0);
-                select.options[0].value = arrayCohorts[i].id;
-                select.options[0].innerText = arrayCohorts[i].id;
-            }
-   }
-    };
+const getProgress = () => {
 
-    xhttp.open("GET", "../data/cohorts.json", true);
-    xhttp.send();
-};
-function computeUsersStats() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState === 4 && xhttp.status === 200) {
-
-            let arrayUsers = JSON.parse(xhttp.responseText);
-            let select = document.getElementById("cboUsers"); 
-            for (let i in arrayUsers) {
-                let option = document.createElement("option");
-                select.options.add(option, 0);
-                select.options[0].value = arrayUsers[i].id;
-                select.options[0].innerText = arrayUsers[i].name;
-            }
-        }
-    };
-    xhttp.open("GET", "../data/cohorts/lim-2018-03-pre-core-pw/users.json", true);
-    xhttp.send();
+	let select = document.getElementById("cboUsers");
+	let arrayProgress = JSON.parse(event.target.responseText);
+	let arrayUnits = arrayProgress[select.value]['intro']['units'];
+	document.getElementById("progressTable").innerText = null;
+	for (let i in arrayUnits) {
+		let fila = "<tr><td>" + i + "</td><td>" + arrayUnits[i].totalParts + "</td></tr>";
+		let btn = document.createElement("TR");
+		btn.innerHTML = fila;
+		document.getElementById("progressTable").appendChild(btn);
+	}
 };
 
-function getProgress() {
 
-    let select = document.getElementById("cboUsers");
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState === 4 && xhttp.status === 200) {
-                                                                  
-            let arrayProgress = JSON.parse(xhttp.responseText);
-            let arrayUnits = arrayProgress[select.value]['intro']['units'];
-            document.getElementById("progressTable").innerText = null;
-            for (let i in arrayUnits) {
-                console.log(i);
-                let fila = "<tr><td>" + i + "</td><td>" + arrayUnits[i].totalParts + "</td></tr>";
-                let btn = document.createElement("TR");
-                btn.innerHTML = fila;
-                document.getElementById("progressTable").appendChild(btn);
-            }
-        }
-    };
-    xhttp.open("GET", "../data/cohorts/lim-2018-03-pre-core-pw/progress.json", true);
-    xhttp.send();
-};
-
+dataRequest(cohortUrl, getCohort);
+dataRequest(userUrl, getUsers);
+dataRequest(progress, getProgress)
